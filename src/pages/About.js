b4fad0074aac72c7e0176230ea93291d7e6c2120
jacklocks <castets.eric@free.fr>
2023-10-05 09:09:32 +0200

    about2

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,17 +12,18 @@ const About = () => {
           <div className="about-text">
             <p>
               Je m'appelle Éric Castets, et ma carrière professionnelle est un
-              parcours diversifié qui m'a conduit à une passionnante reconversion
-              en tant que développeur web. À l'âge de 48 ans, j'ai relevé le
-              défi d'un bootcamp intensif de 8 mois chez The Hacking Project, où
-              j'ai acquis des compétences avancées en développement web,
-              obtenant le niveau 5 du RNCP en tant que développeur web.
+              parcours diversifié qui m'a conduit à une passionnante
+              reconversion en tant que développeur web. À l'âge de 48 ans, j'ai
+              relevé le challenge d'un bootcamp intensif de 8 mois chez The
+              Hacking Project, où j'ai acquis des compétences avancées en
+              développement web, obtenant le niveau 5 du RNCP en tant que
+              développeur web.
               <br />
               Fort de ces compétences techniques fraîchement acquises, j'ai
               découvert ma véritable passion dans le développement web et la
               création d'applications. Mon parcours atypique témoigne de mon
               engagement, ma persévérance et ma capacité d'adaptation face aux
-              défis. Aujourd'hui, je suis prêt à mettre en valeur ma polyvalence
+              défis. Aujourd'hui, je suis prêt à mettre à profit ma polyvalence
               et mon expertise à travers des projets innovants.
               <br />
               Mon portfolio reflète ma progression professionnelle, mettant en
@@ -31,7 +32,7 @@ const About = () => {
               mesure. Avec ma solide expérience entrepreneuriale et mes
               compétences techniques, je suis enthousiaste à l'idée de
               contribuer à des projets passionnants et de collaborer avec des
-              équipes dynamiques pour façonner l'avenir du développement web.
+              équipes dynamiques.
             </p>
           </div>
           <h1>mes compétences</h1>
